b94e1295dbc06c44df9df3a3aeebeff74a32b3c8
root <3290@holbertonschool.com>
2021-11-08 08:50:37 -0800

    pycode

--- models/base_model.py
+++ models/base_model.py
@@ -4,15 +4,16 @@
 import uuid
 import datetime
 
+
 class BaseModel():
     """Base Model"""
     def __init__(self):
-        self.id =  str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        return ("[{}] ({}) {}".format (self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         self.updated_at = datetime.datetime.now()
