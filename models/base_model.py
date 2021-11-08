53427a6d5bc31404c9ee331f961f97ba6edec8e7
boliolagustin <bolioliagustin@gmail.com>
2021-11-08 08:39:35 -0800

    s

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,8 @@ class BaseModel():
     """Base Model"""
     def __init__(self):
         self.id =  str(uuid.uuid4())
-        self.created_at =  datetime.datetime.now()
-        self.updated_at =  datetime.datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}".format (self.__class__.__name__, self.id, self.__dict__))
@@ -18,4 +18,8 @@ class BaseModel():
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        return (BaseModel.__dict__)
+        new_dict = dict(self.__dict__)
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
