1a7a4f5d3e06b619bdb6fdd162a592c63ce476f7
boliolagustin <bolioliagustin@gmail.com>
2021-11-08 13:28:39 -0800

    s

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 """ Storage Json"""
 
-class FileStorage:
-    __file_path = file.json
-    __objects = {}
+import json
+
+
+class FileStorage():
+    def __init__(self):
+        self.__file_path = file.json
+        self.__objects = {}
+
 
     def all(self):
         return (self.__objects)
 
     def new(self, obj):
-    __objects = type(obj).id
+        n = str(obj.__class__.__name__) + "." + str(obj.id)
+
+
+
+    def save(self):
+        with open(file.json,"w") as f:
+            save_json = json.dumps(self.__objects)
+            return f.write(save_json)
+
