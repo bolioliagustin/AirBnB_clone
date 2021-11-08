bf518097d50eb67acf9ba7d49abfe1065f30a17b
boliolagustin <bolioliagustin@gmail.com>
2021-11-08 10:34:08 -0800

    4fin

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,17 @@ import datetime
 
 
 class BaseModel():
-    """Base Model"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
