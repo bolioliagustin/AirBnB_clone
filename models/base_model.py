f8d03affffb4bded0a0884f967aa3e5c861967dc
boliolagustin <bolioliagustin@gmail.com>
2021-11-08 08:43:50 -0800

    s

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ class BaseModel():
     def __init__(self):
         self.id =  str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         return ("[{}] ({}) {}".format (self.__class__.__name__, self.id, self.__dict__))
