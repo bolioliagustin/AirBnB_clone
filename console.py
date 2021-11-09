7a0fa1823c45eca02fbe9395ad18008db9d98fd0
root <3290@holbertonschool.com>
2021-11-09 06:59:50 -0800

    fin6console

--- console.py
+++ console.py
@@ -5,5 +5,14 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """Prompt Project Airbnb"""
     prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        'Quit command to exit the program'
+        return exit()
+
+    def do_EOF(self, line):
+        'Quit command to exit the program'
+        return True
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
